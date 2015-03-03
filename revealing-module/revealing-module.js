75e16799ab6638b6a7d81aa7b067b9d349612def
Arnelle Balane <arnellebalane@gmail.com>
2015-03-03 15:08:44 +0800

    added revealing module pattern

---
+++ revealing-module/revealing-module.js
@@ -0,0 +1,29 @@
+/**
+ * the revealing module pattern
+ * simply defining all the functions and variables, private or public,
+ * inside the scope of the module and just return an anonymous object
+ * with pointers to these members that we wish to reveal as public
+ *
+ * advantages:
+ *    - consistency in the public api
+ *    - it is clear in the end of the module which members are public
+ */
+var module = (function() {
+    var private_greeting = 'Greetings, world!';
+    var public_greeting = 'Hello, world!';
+
+    function greet() {
+        console.log(public_greeting);
+    }
+
+    function set_greeting(greeting) {
+        public_greeting = greeting;
+    }
+
+    function get_greeting() {
+        return private_greeting;
+    }
+
+    return { greet: greet, set: set_greeting };
+});
+
